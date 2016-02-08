5e05a11c70fb4e957bed5f51187eac1d56d14ad5
Jonathan Robinson <jayrowe86@gmail.com>
2016-02-08 01:45:22 -0600

    started layout for aaulyp board page

---
+++ resources/views/pages/board.blade.php
@@ -0,0 +1,183 @@
+@extends('layouts.master')
+
+@section('content')
+        <!-- WRAPPER -->
+<div class="wrapper">
+    <!-- BREADCRUMBS -->
+    <div class="page-header">
+        <div class="container">
+            <h1 class="page-title pull-left">Our Team</h1>
+            <ol class="breadcrumb">
+                <li><a href="{{ url('/board') }}">Team</a></li>
+                <li class="active">Our Team</li>
+            </ol>
+        </div>
+    </div>
+    <!-- END BREADCRUMBS -->
+    <!-- PAGE CONTENT -->
+    <div class="page-content">
+        <div class="container">
+            <h2 class="section-heading">EXECUTIVE BOARD</h2>
+            <!-- BOARD -->
+            <section class="team">
+                <div class="section-content">
+                    <div class="col-md-5">
+                        <div class="team-member media">
+                            <img src="assets/img/team/person1.png" class="media-object img-circle pull-left" alt="" />
+                            <div class="media-body">
+                                <h3 class="media-heading team-name">Omari Montique</h3>
+                                <strong>President</strong>
+                                <hr class="pull-left">
+                                <div class="clearfix"></div>
+                                <p>Dynamically evolve client-based portals through world-class models. Phosfluorescently simplify cross-platform convergence and multimedia based portals. Rapidiously.</p>
+                                <ul class="list-inline social-icon">
+                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5 col-md-offset-2">
+                        <div class="team-member media">
+                            <img src="assets/img/team/person2.png" class="media-object img-circle pull-left" alt="" />
+                            <div class="media-body">
+                                <h3 class="media-heading team-name">Erika Crespo</h3>
+                                <strong>Vice President</strong>
+                                <hr class="pull-left">
+                                <div class="clearfix"></div>
+                                <p>Dynamically evolve client-based portals through world-class models. Phosfluorescently simplify cross-platform convergence and multimedia based portals. Rapidiously.</p>
+                                <ul class="list-inline social-icon">
+                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="team-member media">
+                            <img src="assets/img/team/person3.png" class="media-object img-circle pull-left" alt="" />
+                            <div class="media-body">
+                                <h3 class="media-heading team-name">Nachele Grooms</h3>
+                                <strong>Secretary</strong>
+                                <hr class="pull-left">
+                                <div class="clearfix"></div>
+                                <div class="col-md-4 row">
+                                    <p>Dynamically evolve client-based portals through world-class models. Phosfluorescently simplify cross-platform convergence and multimedia based portals. Rapidiously.</p>
+                                </div>
+                                <div class="clearfix"></div>
+                                <ul class="list-inline social-icon">
+                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- END TEAM -->
+
+            <h2 class="section-heading">COMMITTEE CHAIRS</h2>
+            <!-- TEAM -->
+            <section class="team">
+                <div class="section-content">
+                    <div class="col-md-4">
+                        <div class="team-member media">
+                            <img src="assets/img/team/person1.png" class="media-object img-circle pull-left" alt="" />
+                            <div class="media-body">
+                                <h3 class="media-heading team-name">Jonathan Robinson</h3>
+                                <strong>Communications</strong>
+                                <hr class="pull-left">
+                                <div class="clearfix"></div>
+                                <p>Dynamically evolve client-based portals through world-class models. Phosfluorescently simplify cross-platform convergence and multimedia based portals. Rapidiously.</p>
+                                <ul class="list-inline social-icon">
+                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="team-member media">
+                            <img src="assets/img/team/person2.png" class="media-object img-circle pull-left" alt="" />
+                            <div class="media-body">
+                                <h3 class="media-heading team-name">Chris Tolbert</h3>
+                                <strong>Political</strong>
+                                <hr class="pull-left">
+                                <div class="clearfix"></div>
+                                <p>Dynamically evolve client-based portals through world-class models. Phosfluorescently simplify cross-platform convergence and multimedia based portals. Rapidiously.</p>
+                                <ul class="list-inline social-icon">
+                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="team-member media">
+                            <img src="assets/img/team/person3.png" class="media-object img-circle pull-left" alt="" />
+                            <div class="media-body">
+                                <h3 class="media-heading team-name">Alexis Reddick</h3>
+                                <strong>Community Outreach</strong>
+                                <hr class="pull-left">
+                                <div class="clearfix"></div>
+                                <p>Dynamically evolve client-based portals through world-class models. Phosfluorescently simplify cross-platform convergence and multimedia based portals. Rapidiously.</p>
+                                <ul class="list-inline social-icon">
+                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="team-member media">
+                            <img src="assets/img/team/person4.png" class="media-object img-circle pull-left" alt="" />
+                            <div class="media-body">
+                                <h3 class="media-heading team-name">Ashley Jenkins</h3>
+                                <strong>Membership</strong>
+                                <hr class="pull-left">
+                                <div class="clearfix"></div>
+                                <p>Dynamically evolve client-based portals through world-class models. Phosfluorescently simplify cross-platform convergence and multimedia based portals. Rapidiously.</p>
+                                <ul class="list-inline social-icon">
+                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="team-member media">
+                            <img src="assets/img/team/person4.png" class="media-object img-circle pull-left" alt="" />
+                            <div class="media-body">
+                                <h3 class="media-heading team-name">Terrance Hadnot</h3>
+                                <strong>Fundraising</strong>
+                                <hr class="pull-left">
+                                <div class="clearfix"></div>
+                                <p>Dynamically evolve client-based portals through world-class models. Phosfluorescently simplify cross-platform convergence and multimedia based portals. Rapidiously.</p>
+                                <ul class="list-inline social-icon">
+                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+    <!-- END PAGE CONTENT -->
+    @include('partials.footer')
+</div>
+<!-- END WRAPPER -->
+@stop
