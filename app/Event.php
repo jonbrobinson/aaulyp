15c2c60e8d3e2cd4c787fb6a00371a2b866fe980
Jonathan Robinson <jayrowe86@gmail.com>
2016-03-21 01:41:40 -0500

    added Photos Relationship to Event Model

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    protected $table = 'events';
+
+//    protected $fillable = [''];
+
+    /**
+     * An Event is composed of many photos
+     *
+     * @return object
+     */
+    public function photos()
+    {
+        return $this->hasMany('App\Event_Photo');
+    }
 }
