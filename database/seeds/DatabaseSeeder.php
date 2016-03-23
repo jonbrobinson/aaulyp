46382eefa53a07fa066688cae85e2ced4e9fc603
Jonathan Robinson <jayrowe86@gmail.com>
2016-03-22 20:47:30 -0500

    added EventPhotoTableSeeder to DBTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call(EventTableSeeder::class);
+        $this->call(EventPhotoTableSeeder::class);
 
         Model::reguard();
     }
