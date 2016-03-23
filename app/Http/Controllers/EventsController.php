6c60e0ce60c3e38cb837da69111f15042915b01b
Jonathan Robinson <jayrowe86@gmail.com>
2016-03-22 20:53:31 -0500

    store Event created from EventController

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Aaulyp\Tools\Calendar;
+use App\Aaulyp\Tools\DateHelper;
 use App\Aaulyp\Tools\Locations;
+use App\Event;
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
+use App\Http\Requests\EventRequest;
 use App\Http\Controllers\Controller;
 
 class EventsController extends Controller
 {
+    protected $states;
+    protected $calendarArrays;
+    protected $dateHelper;
+
     /**
      * Display a listing of all events
      *
@@ -26,37 +32,57 @@ class EventsController extends Controller
      */
     public function create()
     {
-        $locations = new Locations();
-        $calendar = new Calendar();
-
-
-        $states = $locations->getStates();
-        $calendarArrays = $calendar->getCalendarArrays();
-        $data  = [
-            "states" => $states,
-            "calendar" => $calendarArrays
-        ];
+        $data  = $data  = $this->getEventFormData();
 
         return view('pages.events.create',$data );
     }
 
     /**
      * Store newly created resource in storage
-     *
+     * @param EventRequest $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function store(Request $request)
+    public function store(EventRequest $request)
+    {
+        $this->eventSetUp();
+
+        // validate the form
+        // persist the flyer
+        //redirect to the landing page
+        $event = new Event();
+        $event->name = $request->input('event-name');
+        $event->description = $request->input('event-description');
+        $event->street = $request->input('event-street');
+        $event->city = $request->input('event-city');
+        $event->state = $request->input('event-state');
+        $event->zip = $request->input('event-zip');
+        $event->date_start = $this->dateHelper->getStartTimeFromRange($request->input('daterangepicker'));
+        $event->date_end = $this->dateHelper->getEndTimeFromRange($request->input('daterangepicker'));
+
+        $event->save();
+
+
+        return redirect()->back();
+    }
+
+    protected function eventSetUp()
     {
         $locations = new Locations();
-        $calendar = new Calendar();
+        $this->dateHelper = new DateHelper();
 
+        $this->states = $locations->getStates();
+        $this->calendarArrays = $this->dateHelper->getCalendarArrays();
+    }
 
-        $states = $locations->getStates();
-        $calendarArrays = $calendar->getCalendarArrays();
-        $data  = [
-            "states" => $states,
-            "calendar" => $calendarArrays
+    protected function getEventFormData()
+    {
+        $this->eventSetUp();
+
+        $data = [
+            "states" => $this->states,
+            "calendar" => $this->calendarArrays
         ];
 
-        return view('pages.events.create',$data );
+        return $data;
     }
 }
