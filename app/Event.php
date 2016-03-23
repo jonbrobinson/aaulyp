cbdd686dfc09428652f5597dab53e4a29aa2b778
Jonathan Robinson <jayrowe86@gmail.com>
2016-03-22 20:46:41 -0500

    added fillable fields for Event model

--- app/Event.php
+++ app/Event.php
@@ -8,7 +8,21 @@ class Event extends Model
 {
     protected $table = 'events';
 
-//    protected $fillable = [''];
+    /**
+     * Fillable fields for an Event
+     *
+     * @var array
+     */
+    protected $fillable = [
+        "name",
+        "description",
+        "street",
+        "city",
+        "state",
+        "zip",
+        "date_start",
+        "date_end"
+    ];
 
     /**
      * An Event is composed of many photos
