70998cff82b202e932ebd29b71d93383ab1a8147
Jonathan Robinson <jayrowe86@gmail.com>
2016-03-26 14:49:01 -0500

    released authentication layer for show and index methods in EventsController

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -20,7 +20,7 @@ class EventsController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['show','index']]);
     }
 
     /**
