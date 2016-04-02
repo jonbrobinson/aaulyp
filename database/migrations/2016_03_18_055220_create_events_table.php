d6dd7c5aa4930cecf243adf9b0b482e5b8106951
Jonathan Robinson <jayrowe86@gmail.com>
2016-04-01 23:12:59 -0500

    removed foreign key constraint in migrations

--- database/migrations/2016_03_18_055220_create_events_table.php
+++ database/migrations/2016_03_18_055220_create_events_table.php
@@ -14,7 +14,11 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned();
+//            $table->integer('user_id')->unsigned();
+//            $table->foreign('user_id')
+//                ->references('id')
+//                ->on('users')
+//                ->onDelete('cascade');
             $table->string('name');
             $table->text('description');
             $table->string('street');
