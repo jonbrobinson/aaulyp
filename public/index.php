c5c2dfb4ba19eab962bbbfa66a102642705357b5
Jonathan Robinson <jayrowe86@gmail.com>
2016-04-08 22:50:38 -0500

    "simple site readjustment."

--- public/index.php
+++ public/index.php
@@ -1,58 +1,110 @@
-<?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylorotwell@gmail.com>
- */
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels nice to relax.
-|
-*/
+<!DOCTYPE html>
+<html lang="en">
 
-require __DIR__.'/../bootstrap/autoload.php';
+<!--///**-->
+<!--// * Laravel - A PHP Framework For Web Artisans-->
+<!--// *-->
+<!--// * @package  Laravel-->
+<!--// * @author   Taylor Otwell <taylorotwell@gmail.com>-->
+<!--// */-->
+<!--//-->
+<!--///*-->
+<!--//|---------------------------------------------------------------------------->
+<!--//| Register The Auto Loader-->
+<!--//|---------------------------------------------------------------------------->
+<!--//|-->
+<!--//| Composer provides a convenient, automatically generated class loader for-->
+<!--//| our application. We just need to utilize it! We'll simply require it-->
+<!--//| into the script here so that we don't have to worry about manual-->
+<!--//| loading any of our classes later on. It feels nice to relax.-->
+<!--//|-->
+<!--//*/-->
+<!--//-->
+<!--//require __DIR__.'/../bootstrap/autoload.php';-->
+<!--//-->
+<!--///*-->
+<!--//|---------------------------------------------------------------------------->
+<!--//| Turn On The Lights-->
+<!--//|---------------------------------------------------------------------------->
+<!--//|-->
+<!--//| We need to illuminate PHP development, so let us turn on the lights.-->
+<!--//| This bootstraps the framework and gets it ready for use, then it-->
+<!--//| will load up this application so that we can run it and send-->
+<!--//| the responses back to the browser and delight our users.-->
+<!--//|-->
+<!--//*/-->
+<!--//-->
+<!--//$app = require_once __DIR__.'/../bootstrap/app.php';-->
+<!--//-->
+<!--///*-->
+<!--//|---------------------------------------------------------------------------->
+<!--//| Run The Application-->
+<!--//|---------------------------------------------------------------------------->
+<!--//|-->
+<!--//| Once we have the application, we can handle the incoming request-->
+<!--//| through the kernel, and send the associated response back to-->
+<!--//| the client's browser allowing them to enjoy the creative-->
+<!--//| and wonderful application we have prepared for them.-->
+<!--//|-->
+<!--//*/-->
+<!--//-->
+<!--//$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);-->
+<!--//-->
+<!--//$response = $kernel->handle(-->
+<!--//    $request = Illuminate\Http\Request::capture()-->
+<!--//);-->
+<!--//-->
+<!--//$response->send();-->
+<!--//-->
+<!--//$kernel->terminate($request, $response);-->
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
+    <head>
+        <title>Austin Area Urban League Young Professionals</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Austin Area Urban League">
+        <meta name="author" content="The Develovers">
+        <!-- CSS -->
+<!--            <link href="{{ asset("assets/css/bootstrap.css")}}" rel="stylesheet" type="text/css">-->
+<!--        <link href="{{ asset("assets/css/font-awesome.min.css") }}" rel="stylesheet" type="text/css">-->
+<!--        <link href="{{ asset("assets/css/main.css") }}" rel="stylesheet" type="text/css">-->
+<!--        <link href="{{ asset("assets/css/my-custom-styles.css") }}" rel="stylesheet" type="text/css">-->
+<!--        <link href="{{ asset("assets/css/skins/indianred_custom.css") }}" rel="stylesheet" type="text/css">-->
+<!--        <link href="{{ asset("assets/css/custom.css") }}" rel="stylesheet" type="text/css">-->
+<!--        <link href="{{ asset("css/libs.css") }}" rel="stylesheet" type="text/css">-->
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+        <!-- IE 9 Fallback-->
+        <!--[if IE 9]>
+        <!--<link href="assets/css/ie.css" rel="stylesheet">-->
+        <![endif]-->
+        <!-- GOOGLE FONTS -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400italic,400,600,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300,300italic,400italic,700,400,300' rel='stylesheet' type='text/css'>
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+            </head>
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+    <body>
 
-$response->send();
+    <div class="wrapper">
+        <h1>You're in the right place we are just working on few changes.</h1>
 
-$kernel->terminate($request, $response);
+    </div>
+
+
+
+
+            <!-- JAVASCRIPTS -->
+<!--        <script src="{{ asset("assets/js/jquery-2.1.1.min.js") }}"></script>-->
+<!--        <script src="{{ asset("assets/js/bootstrap.min.js") }}"></script>-->
+<!--        <script src="{{ asset("assets/js/plugins/autohidingnavbar/jquery.bootstrap-autohidingnavbar.min.js") }}"></script>-->
+<!--        <script src="{{ asset("assets/js/plugins/slick/slick.min.js") }}"></script>-->
+<!--        <script src="{{ asset("assets/js/plugins/stellar/jquery.stellar.min.js") }}"></script>-->
+<!--        <script src="{{ asset("assets/js/plugins/jquery-easypiechart/jquery.easypiechart.min.js") }}"></script>-->
+<!--        <script src="{{ asset("assets/js/repute-scripts.js") }}"></script>-->
+<!--        <script src="{{ asset("js/libs.js") }}"></script>-->
+    </body>
+
+</html>
