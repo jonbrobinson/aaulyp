fc8eea269bdc4615e554a4d07b379c7ca4158c03
Jonathan Robinson <jayrowe86@gmail.com>
2016-04-10 23:51:30 -0500

    revert index page back to lararevl index page

--- public/index.php
+++ public/index.php
@@ -1,138 +1,58 @@
+<?php
 
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylorotwell@gmail.com>
+ */
 
-<!DOCTYPE html>
-<html lang="en">
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels nice to relax.
+|
+*/
 
-<!--///**-->
-<!--// * Laravel - A PHP Framework For Web Artisans-->
-<!--// *-->
-<!--// * @package  Laravel-->
-<!--// * @author   Taylor Otwell <taylorotwell@gmail.com>-->
-<!--// */-->
-<!--//-->
-<!--///*-->
-<!--//|---------------------------------------------------------------------------->
-<!--//| Register The Auto Loader-->
-<!--//|---------------------------------------------------------------------------->
-<!--//|-->
-<!--//| Composer provides a convenient, automatically generated class loader for-->
-<!--//| our application. We just need to utilize it! We'll simply require it-->
-<!--//| into the script here so that we don't have to worry about manual-->
-<!--//| loading any of our classes later on. It feels nice to relax.-->
-<!--//|-->
-<!--//*/-->
-<!--//-->
-<!--//require __DIR__.'/../bootstrap/autoload.php';-->
-<!--//-->
-<!--///*-->
-<!--//|---------------------------------------------------------------------------->
-<!--//| Turn On The Lights-->
-<!--//|---------------------------------------------------------------------------->
-<!--//|-->
-<!--//| We need to illuminate PHP development, so let us turn on the lights.-->
-<!--//| This bootstraps the framework and gets it ready for use, then it-->
-<!--//| will load up this application so that we can run it and send-->
-<!--//| the responses back to the browser and delight our users.-->
-<!--//|-->
-<!--//*/-->
-<!--//-->
-<!--//$app = require_once __DIR__.'/../bootstrap/app.php';-->
-<!--//-->
-<!--///*-->
-<!--//|---------------------------------------------------------------------------->
-<!--//| Run The Application-->
-<!--//|---------------------------------------------------------------------------->
-<!--//|
-<!--//| Once we have the application, we can handle the incoming request-->
-<!--//| through the kernel, and send the associated response back to-->
-<!--//| the client's browser allowing them to enjoy the creative-->
-<!--//| and wonderful application we have prepared for them.-->
-<!--//|-->
-<!--//*/-->
-<!--//-->
-<!--//$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);-->
-<!--//-->
-<!--//$response = $kernel->handle(-->
-<!--//    $request = Illuminate\Http\Request::capture()-->
-<!--//);-->
-<!--//-->
-<!--//$response->send();-->
-<!--//-->
-<!--//$kernel->terminate($request, $response);-->
+require __DIR__.'/../bootstrap/autoload.php';
 
-    <head>
-        <title>Austin Area Urban League Young Professionals</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Austin Area Urban League">
-        <meta name="author" content="The Develovers">
-        <!-- CSS -->
-<!--            <link href="{{ asset("assets/css/bootstrap.css")}}" rel="stylesheet" type="text/css">-->
-<!--        <link href="{{ asset("assets/css/font-awesome.min.css") }}" rel="stylesheet" type="text/css">-->
-<!--        <link href="{{ asset("assets/css/main.css") }}" rel="stylesheet" type="text/css">-->
-<!--        <link href="{{ asset("assets/css/my-custom-styles.css") }}" rel="stylesheet" type="text/css">-->
-<!--        <link href="{{ asset("assets/css/skins/indianred_custom.css") }}" rel="stylesheet" type="text/css">-->
-<!--        <link href="{{ asset("assets/css/custom.css") }}" rel="stylesheet" type="text/css">-->
-<!--        <link href="{{ asset("css/libs.css") }}" rel="stylesheet" type="text/css">-->
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
 
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-        <!-- Bootstrap For Coming Soon Page -->
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
 
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
-        <!-- IE 9 Fallback-->
-        <!--[if IE 9]>
-        <!--<link href="assets/css/ie.css" rel="stylesheet">-->
-        <![endif]-->
-        <!-- GOOGLE FONTS -->
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400italic,400,600,700' rel='stylesheet' type='text/css'>
-        <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300,300italic,400italic,700,400,300' rel='stylesheet' type='text/css'>
+$response->send();
 
-        <style>
-            .aaulyp-red {color: #a5101e}
-        </style>
-
-    </head>
-
-    <body>
-
-    <div class="wrapper">
-        <div class="container">
-            <br>
-            <br>
-            <br>
-            <div class="row">
-                <img src="UL-black.png" alt="Austin Area Urban League YOung Professionals" class="img-responsive center-block" >
-                <h1 class="text-center">Coming Soon.</h1>
-                <h2 class="text-center">Follow us on
-                    <a href="http://www.facebook.com/aaulyp" class="aaulyp-red"><i class="fa fa-facebook aaulyp-red"></i></a>
-                    <a href="http://www.twitter.com/aaulyp" class="aaulyp-red"><i class="fa fa-twitter aaulyp-red"></i></a>
-                    <a href="http://www.instagram.com/aaulyp" class="aaulyp-red"><i class="fa fa-instagram aaulyp-red"></i></a>
-                    @AAULYP.  #YPEmpowered
-                </h2>
-                <h2 class="text-center">Email: <a href="mailto: pr.aaulyp@gmail.com" class="aaulyp-red">pr.aaulyp@gmail.com</a></h2>
-            </div>
-        </div>
-
-    </div>
-
-
-
-
-            <!-- JAVASCRIPTS -->
-<!--        <script src="{{ asset("assets/js/jquery-2.1.1.min.js") }}"></script>-->
-<!--        <script src="{{ asset("assets/js/bootstrap.min.js") }}"></script>-->
-<!--        <script src="{{ asset("assets/js/plugins/autohidingnavbar/jquery.bootstrap-autohidingnavbar.min.js") }}"></script>-->
-<!--        <script src="{{ asset("assets/js/plugins/slick/slick.min.js") }}"></script>-->
-<!--        <script src="{{ asset("assets/js/plugins/stellar/jquery.stellar.min.js") }}"></script>-->
-<!--        <script src="{{ asset("assets/js/plugins/jquery-easypiechart/jquery.easypiechart.min.js") }}"></script>-->
-<!--        <script src="{{ asset("assets/js/repute-scripts.js") }}"></script>-->
-<!--        <script src="{{ asset("js/libs.js") }}"></script>-->
-    </body>
-
-</html>
+$kernel->terminate($request, $response);
