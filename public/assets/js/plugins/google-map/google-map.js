6c8377c16e8d4ed3d325184164f313389ffd7622
Jonathan Robinson <jayrowe86@gmail.com>
2016-04-12 02:06:18 -0500

    completed custom map with marker for events page

--- public/assets/js/plugins/google-map/google-map.js
+++ public/assets/js/plugins/google-map/google-map.js
@@ -148,9 +148,11 @@
 				var image = new google.maps.MarkerImage("assets/img/location-pin.png", null, null, null, new google.maps.Size(32, 32));
 				var beachMarker = new google.maps.Marker({
 					map: theMap,
-					icon: image,
+					//icon: image,
 					position: results[0].geometry.location
 				});
+
+				beachMarker;
 
 			} else {
 				alert('Geocode was not successful for the following reason: ' + status);
--- resources/views/pages/events/show.blade.php
+++ resources/views/pages/events/show.blade.php
@@ -8,23 +8,22 @@
     <div class="page-content">
         <div class="container">
             <div class="row">
-                <div class="col-sm-6 col-md-4">
-                    <h1>{{ $event->name }}</h1>
+                <div class="col-sm-6 col-md-9">
+                    <h2>{{ $event->name }}</h2>
 
-                    <h2> {!! $event->street !!}</h2>
+                    <h4 id="map-address">{!! $event->street !!}, {!! $event->city !!}, {!! $event->state !!} {!! $event->zip !!}</h4>
 
                     <hr>
 
                     <div>{!! $event->description !!}</div>
 
                 </div>
-                <div class="col-md-8" id="map"></div>
             </div>
             <div class="row">
-                <div class="col-sm-6 col-md-8 gallery">
+                <div class="col-sm-6 col-md-9 gallery">
                     <div class="row">
                         @foreach($event->photos as $photo)
-                            <div class="col-xs-4 col-md-3 gallery_image">
+                            <div class="col-xs-4 col-md-2 gallery_image">
                                 <a href="{{ asset("$photo->path") }}" data-lightbox="event-show"><img src="{{ asset("$photo->thumbnail_path") }}" alt="" class="img-responsive"></a>
                             </div>
                         @endforeach
@@ -43,6 +42,10 @@
                         </div>
                     @endif
                 </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-9" id="map"></div>
             </div>
         </div>
     </div>
@@ -67,11 +70,145 @@
         }
     </script>
     <script>
-        var map;
         function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: -34.397, lng: 150.644},
-                zoom: 8
+            geocoder = new google.maps.Geocoder();
+
+            var styles = [
+                {
+                    "stylers": [
+                        { "visibility": "on" }
+                    ]
+                },
+                {
+                    "featureType": "landscape.natural",
+                    "stylers": [
+                        { "visibility": "simplified" },
+                        { "color": "#f0f0f0" }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "stylers": [
+                        { "visibility": "simplified" },
+                        { "color": "#C2E7F5" }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "geometry",
+                    "stylers": [
+                        { "visibility": "simplified" },
+                        { "color": "#ffffff" }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "elementType": "geometry.stroke",
+                    "stylers": [
+                        { "visibility": "off" }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        { "visibility": "off" }
+                    ]
+                },
+                {
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        { "visibility": "on" },
+                        { "color": "#646464" }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        { "visibility": "on" },
+                        { "weight": 1 },
+                        { "color": "#ffffff" }
+                    ]
+                },
+                {
+                    "featureType": "poi.park",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        { "lightness": 90 },
+                        { "color": "#d7d7d7" },
+                        { "visibility": "off" }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "elementType": "geometry",
+                    "stylers": [
+                        { "visibility": "on" },
+                        { "color": "#ffffff" }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        { "visibility": "on" },
+                        { "color": "#b8b8b8" }
+                    ]
+                },
+                {
+                    "featureType": "landscape.man_made",
+                    "elementType": "geometry",
+                    "stylers": [
+                        { "visibility": "on" },
+                        { "lightness": 60 },
+                        { "saturation": -90 },
+                        { "gamma": 0.90 }
+                    ]
+                }
+            ];
+
+            var styledMap = new google.maps.StyledMapType(styles, {
+                name: "Styled Map"
+            });
+
+            var mapOptions = {
+                zoom: 14,
+                scrollwheel: false,
+                panControl: false,
+                scaleControl: false,
+                mapTypeControlOptions: {
+                    mapTypeIds: []
+                }
+            };
+
+            var mapPlaceholder = document.getElementById('map');
+
+            if(mapPlaceholder) {
+                customMap = new google.maps.Map(mapPlaceholder, mapOptions);
+                customMap.mapTypes.set('map_style', styledMap);
+                customMap.setMapTypeId('map_style');
+                codeAddress(customMap);
+            }
+        }
+
+        function codeAddress(theMap) {
+
+            var address = document.getElementById('map-address').innerText;
+            console.log(address);
+            geocoder.geocode( { 'address': address}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    theMap.setCenter(results[0].geometry.location);
+                    var image = new google.maps.MarkerImage("{{ asset('assets/img/location-pin.png') }}", null, null, null, new google.maps.Size(32, 32));
+                    var beachMarker = new google.maps.Marker({
+                        map: theMap,
+                        icon: image,
+                        position: results[0].geometry.location
+                    });
+
+                } else {
+                    alert('Geocode was not successful for the following reason: ' + status);
+                }
             });
         }
     </script>
