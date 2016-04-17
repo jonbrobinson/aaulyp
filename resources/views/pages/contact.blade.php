8a2c59174f863c1d510de985b81e40ad4fc47971
Jonathan Robinson <jayrowe86@gmail.com>
2016-04-17 15:40:40 -0500

    added error feedback to contact form

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -111,6 +111,7 @@
                     data: $("#the-form").serialize(), // serializes the form's elements.
                     success: function(data)
                     {
+                        resetResponse();
                         var success = '<p class=\'alert alert-success\'>' + data.message + '<a href=\'#\' class=\'close\' data-dismiss=\'alert\' aria-label=\'close\'>&times;</a></p>';
                         $('#server-response').append(success);
                         $('#contact-name').val('');
@@ -118,13 +119,32 @@
                         $('#contact-subject').val('');
                         $('#contact-message').val('');
                     },
-//                    error: function(xhr, status, error) {
-//                        var
-//                        console.log(xhr);
-//                    }
+                    error: function(xhr, status, error) {
+                        resetResponse();
+                        console.log(xhr.responseJSON.errors);
+                        var errorList = '';
+                        var errors = xhr.responseJSON.errors;
+
+                        for (index = 0; index < errors.length; ++index) {
+                            errorList += '<li>' + errors[index] + '</li>';
+                        }
+
+                        var errorElem = '<ul>' + errorList + '</ul>';
+
+                        $('#server-response').addClass('alert alert-danger').append(errorElem);
+
+                    }
                 });
 
                 e.preventDefault(); // avoid to execute the actual submit of the form.
+
+                function resetResponse (){
+                    var emptyDiv= $('#server-response').empty();
+
+                    if (emptyDiv.hasClass('alert alert-danger')) {
+                        emptyDiv.removeClass('alert alert-danger');
+                    }
+                };
             });
         });
     </script>
