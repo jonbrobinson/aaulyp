00a8967035eaf48bb482798d8048e75de38ba910
Jonathan Robinson <jayrowe86@gmail.com>
2016-04-27 00:56:19 -0500

    restructure join page to add more membership information

--- resources/views/pages/join.blade.php
+++ resources/views/pages/join.blade.php
@@ -19,31 +19,46 @@
         <div class="container">
             <section>
                 <div class="col-md-12 row">
-                    <div class="col-md-6 ">
-                        <h2 class="section-heading">Join Now</h2>
+                    <div class="col-md-12 row">
+                        <p class="lead">Join a community that is empowering Austin and connecting young professionals around the Austin Area with same values and commitment to helping others.</p>
+                        <p class="lead">Membership is an annual commitment and  runs on a 12 month cycle from the time of purchase. Please contact our member ship chair Ashley Jenkins at <a href="mailto:membership.aaulyp@gmail.com">membership.aaulyp@gmail.com</a></p>
+                    </div>
+                    <div class="col-md-6 row">
+                        <div>
+                            <h2 class="section-heading">Membership Benefits</h2>
+                            <div class="col-md-12 row">
+                                <h4><span class="favicon-list-custom"><i class="fa fa-check-circle fa-lg"></i></span> Partnering with us to impact our generation, empower communities and change lives </h4>
+                                <h4><span class="favicon-list-custom"><i class="fa fa-check-circle fa-lg"></i></span> Invitations and discounts to YP events and YP partner organization events</h4>
+                                <h4><span class="favicon-list-custom"><i class="fa fa-check-circle fa-lg"></i></span> Invitations to Austin Urban League events</h4>
+                                <h4><span class="favicon-list-custom"><i class="fa fa-check-circle fa-lg"></i></span> Discounts to Austin Area Urban League and Austin Area Urban League Young Professional Events</h4>
+                                <h4><span class="favicon-list-custom"><i class="fa fa-check-circle fa-lg"></i></span> Discounts to National Urban League and National Urban League Young Professional Events</h4>
+                                <h4><span class="favicon-list-custom"><i class="fa fa-check-circle fa-lg"></i></span> Austin Area Urban League Young Professionals Shirt</h4>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <br>
+                        <br>
                         <div class="col-md-11 row">
-                            <p>Joining the YP is easy. Please purchase a member from our Eventbrite.</p>
-                            <p><a href="http://www.eventbrite.com/e/aaulyp-membership-tickets-5422224018?aff=es2" class="active btn btn-primary">Join on Eventbrite</a></p>
-                            <p>From there, we encourage you to get involved by coming to our General Body Meetings (GBM), and volunteering at some of our service opportunities. Our committee chairs (Membership, Fundraising, Community Outreach, etc) are also looking for people to help plan and organize some of the events we host.</p>
+                            <p class="text-center"><a href="http://www.eventbrite.com/e/aaulyp-membership-tickets-5422224018?aff=es2" class="active btn btn-primary btn-lg">Join Now</a></p>
                         </div>
                     </div>
                     <div class="col-md-6 row">
-                        <div class="col-md-6">
+                        <div class="col-xs-6">
                             <img src="{{ asset('assets/img/aaulyp/img_800x800/gcp_garden_800x800.png') }}" alt="" class="img-responsive">
                             <br>
                             <br>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-xs-6">
                             <img src="{{ asset('assets/img/aaulyp/img_800x800/heritage_fest_group_800x800.png') }}" alt="" class="img-responsive">
                             <br>
                             <br>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-xs-6">
                             <img src="{{ asset('assets/img/aaulyp/img_800x800/omni_pres_800x800.png') }}" alt="" class="img-responsive">
                             <br>
                             <br>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-xs-6">
                             <img src="{{ asset('assets/img/aaulyp/img_800x800/gbm_secretary_800x800.png') }}" alt="" class="img-responsive">
                             <br>
                             <br>
@@ -52,26 +67,6 @@
                 </div>
 
             </section>
-
-            <div class="clearfix"></div>
-
-            <div class="row">
-
-                <div class="col-md-6">
-                    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F20531316728%2Fposts%2F10154009990506729%2F&width=500&show_text=false&height=290&appId" width="500" height="290" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>                </div>
-
-                <div class="col-md-6">
-                    <h2 class="section-heading">Membership Benefits</h2>
-                    <div class="col-md-12 row">
-                        <h5><span class="favicon-list-custom"><i class="fa fa-check-circle"></i></span> Austin Area Urban League Young Professionals Shirt</h5>
-                        <h5><span class="favicon-list-custom"><i class="fa fa-check-circle"></i></span> Invitations and discounts to YP events and YP partner organization events</h5>
-                        <h5><span class="favicon-list-custom"><i class="fa fa-check-circle"></i></span> Invitations and discounts to Austin Urban League events</h5>
-                        <h5><span class="favicon-list-custom"><i class="fa fa-check-circle"></i></span> Member Only Events</h5>
-                        <h5><span class="favicon-list-custom"><i class="fa fa-check-circle"></i></span> Member Only Discounts</h5>
-                        <h5><span class="favicon-list-custom"><i class="fa fa-check-circle"></i></span> Partnering with us to impact our generation, empower communities and change lives </h5>
-                    </div>
-                </div>
-            </div>
 
         </div>
     </div>
