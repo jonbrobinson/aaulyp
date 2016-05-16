85d7c85375b648bea0c02f5a32819a13c49e3afc
Jonathan Robinson <jayrowe86@gmail.com>
2016-05-15 22:01:22 -0500

    create custom 404 page to match site styling

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="page-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <br>
+                    <br>
+                    <h1>PAGE NOT FOUND</h1>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    @include('partials.footer')
+@stop
