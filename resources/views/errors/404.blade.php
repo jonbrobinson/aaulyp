9952dbb1e89a9b505d52098dc00c0abbf01d449f
Jonathan Robinson <jayrowe86@gmail.com>
2016-05-24 00:07:48 -0500

    update 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="page-content">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <br>
-                    <br>
-                    <h1>PAGE NOT FOUND</h1>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
+        <!-- PAGE CONTENT -->
+<div class="page-content page-error text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <h1>404</h1>
+                <h2>OOPS! PAGE NOT FOUND</h2>
+                <hr />
+                <div class="error-description">
+                    <p>The page you were looking for could not be found.</p>
                 </div>
             </div>
         </div>
-
     </div>
+</div>
+<!-- END PAGE CONTENT -->
     @include('partials.footer')
 @stop
