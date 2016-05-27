d3be0e4355a8d5ac32901407636fe076d7d798b6
Jonathan Robinson <jayrowe86@gmail.com>
2016-05-27 11:54:14 -0500

    added analytics to web pages

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -50,6 +50,16 @@
         <script src="{{ asset("assets/js/repute-scripts.js") }}"></script>
         <script src="{{ asset("js/libs.js") }}"></script>
         @include('partials.flash')
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-78412288-1', 'auto');
+            ga('send', 'pageview');
+
+        </script>
     </body>
 
 </html>
