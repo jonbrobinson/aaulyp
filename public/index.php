0b6369749532e9726d21d7f1020c9b8063b244dc
Jonathan Robinson <jayrowe86@gmail.com>
2016-05-28 03:52:36 -0500

    remove commented code

--- public/index.php
+++ public/index.php
@@ -3,77 +3,12 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!--///**-->
-<!--// * Laravel - A PHP Framework For Web Artisans-->
-<!--// *-->
-<!--// * @package  Laravel-->
-<!--// * @author   Taylor Otwell <taylorotwell@gmail.com>-->
-<!--// */-->
-<!--//-->
-<!--///*-->
-<!--//|---------------------------------------------------------------------------->
-<!--//| Register The Auto Loader-->
-<!--//|---------------------------------------------------------------------------->
-<!--//|-->
-<!--//| Composer provides a convenient, automatically generated class loader for-->
-<!--//| our application. We just need to utilize it! We'll simply require it-->
-<!--//| into the script here so that we don't have to worry about manual-->
-<!--//| loading any of our classes later on. It feels nice to relax.-->
-<!--//|-->
-<!--//*/-->
-<!--//-->
-<!--//require __DIR__.'/../bootstrap/autoload.php';-->
-<!--//-->
-<!--///*-->
-<!--//|---------------------------------------------------------------------------->
-<!--//| Turn On The Lights-->
-<!--//|---------------------------------------------------------------------------->
-<!--//|-->
-<!--//| We need to illuminate PHP development, so let us turn on the lights.-->
-<!--//| This bootstraps the framework and gets it ready for use, then it-->
-<!--//| will load up this application so that we can run it and send-->
-<!--//| the responses back to the browser and delight our users.-->
-<!--//|-->
-<!--//*/-->
-<!--//-->
-<!--//$app = require_once __DIR__.'/../bootstrap/app.php';-->
-<!--//-->
-<!--///*-->
-<!--//|---------------------------------------------------------------------------->
-<!--//| Run The Application-->
-<!--//|---------------------------------------------------------------------------->
-<!--//|
-<!--//| Once we have the application, we can handle the incoming request-->
-<!--//| through the kernel, and send the associated response back to-->
-<!--//| the client's browser allowing them to enjoy the creative-->
-<!--//| and wonderful application we have prepared for them.-->
-<!--//|-->
-<!--//*/-->
-<!--//-->
-<!--//$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);-->
-<!--//-->
-<!--//$response = $kernel->handle(-->
-<!--//    $request = Illuminate\Http\Request::capture()-->
-<!--//);-->
-<!--//-->
-<!--//$response->send();-->
-<!--//-->
-<!--//$kernel->terminate($request, $response);-->
-
     <head>
         <title>Austin Area Urban League Young Professionals</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Austin Area Urban League">
-        <meta name="author" content="The Develovers">
-        <!-- CSS -->
-<!--            <link href="{{ asset("assets/css/bootstrap.css")}}" rel="stylesheet" type="text/css">-->
-<!--        <link href="{{ asset("assets/css/font-awesome.min.css") }}" rel="stylesheet" type="text/css">-->
-<!--        <link href="{{ asset("assets/css/main.css") }}" rel="stylesheet" type="text/css">-->
-<!--        <link href="{{ asset("assets/css/my-custom-styles.css") }}" rel="stylesheet" type="text/css">-->
-<!--        <link href="{{ asset("assets/css/skins/indianred_custom.css") }}" rel="stylesheet" type="text/css">-->
-<!--        <link href="{{ asset("assets/css/custom.css") }}" rel="stylesheet" type="text/css">-->
-<!--        <link href="{{ asset("css/libs.css") }}" rel="stylesheet" type="text/css">-->
+        <meta name="author" content="Jonbrobinson.com">
 
 
         <!-- Bootstrap For Coming Soon Page -->
@@ -120,19 +55,6 @@
         </div>
 
     </div>
-
-
-
-
-            <!-- JAVASCRIPTS -->
-<!--        <script src="{{ asset("assets/js/jquery-2.1.1.min.js") }}"></script>-->
-<!--        <script src="{{ asset("assets/js/bootstrap.min.js") }}"></script>-->
-<!--        <script src="{{ asset("assets/js/plugins/autohidingnavbar/jquery.bootstrap-autohidingnavbar.min.js") }}"></script>-->
-<!--        <script src="{{ asset("assets/js/plugins/slick/slick.min.js") }}"></script>-->
-<!--        <script src="{{ asset("assets/js/plugins/stellar/jquery.stellar.min.js") }}"></script>-->
-<!--        <script src="{{ asset("assets/js/plugins/jquery-easypiechart/jquery.easypiechart.min.js") }}"></script>-->
-<!--        <script src="{{ asset("assets/js/repute-scripts.js") }}"></script>-->
-<!--        <script src="{{ asset("js/libs.js") }}"></script>-->
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
