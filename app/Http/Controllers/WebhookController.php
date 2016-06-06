cec97ea16898b19d5dddaaec353d2b307bc93110
Jonathan Robinson <jayrowe86@gmail.com>
2016-06-06 10:44:51 -0500

    test response

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -21,13 +21,15 @@ class WebhookController extends Controller
     public function ebOrders(Request $request)
     {
 
+        return response('made it here');
+        
         $this->init();
 
         $orderUrl = $request->input('api_url');
 
         $orderUser = $this->eventbrite->getOrderPlaced($orderUrl);
 
-        $response = $this->emailer->sendWelcomeEmail($orderUser);
+//        $response = $this->emailer->sendWelcomeEmail($orderUser);
 
         if ($response->getStatusCode() == 200) {
             return response()->json([
