35117a313b81cb8bc8b21ba044eccd5479839a8d
Jonathan Robinson <jayrowe86@gmail.com>
2016-06-10 08:13:29 -0500

    add yp email blade

---
+++ resources/views/pages/emails/ypWeekendOrdersUpdateEmail.blade.php
@@ -0,0 +1,14 @@
+<h2><strong>Order Confirmed</strong></h2>
+
+<p>A new order has been placed</p>
+
+<p>Order count: {{ $orderCount or 0 }}</p>
+
+
+<p>--<br><em>Yours in the movement</em></p>
+<p>
+    <strong>Jonathan Robinson</strong><br>
+    Communication Chair | Austin Area Urban League Young Professionals<br>
+    <a href="http://www.facebook.com/aaulyp">Facebook</a> | <a href="http://www.twitter.com/aaulyp">Twitter</a> | <a href="http://www.instagram.com/aaulyp">Instagram</a> | <a href="http://www.aaulyp.org">Aaulyp.org</a> <br>
+</p>
+<img src="http://s32.postimg.org/5w9yuk785/UL_black.png" alt="" style="max-width: 200px">
