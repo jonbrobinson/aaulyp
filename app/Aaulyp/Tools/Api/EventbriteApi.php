e7eebf368a9f48d8aa6569a2b696bef717b8eb9d
Jonathan Robinson <jayrowe86@gmail.com>
2016-06-14 00:55:35 -0500

    update ticket classes price

--- app/Aaulyp/Tools/Api/EventbriteApi.php
+++ app/Aaulyp/Tools/Api/EventbriteApi.php
@@ -137,15 +137,21 @@ class EventbriteApi
      */
     protected function convertTicketsInfo($tickets)
     {
-        $json = json_decode($tickets);
+        $json = json_decode($tickets, true);
 
         $ticketsInfo= array();
 
         foreach ($json->{'ticket_classes'} as $key => $ticketClass) {
             $ticketInfo = array();
             $ticketInfo['name']  = $ticketClass->name;
-//            $ticketInfo['price'] = $ticketClass->{'actual_cost'}->value;
             $ticketInfo['sold']  = $ticketClass->{'quantity_sold'};
+
+            if ($ticketClass->name == 'Donation') {
+                $ticketInfo['price'] = "donation";
+            } else {
+                $ticketInfo['price'] = $ticketClass->{'actual_cost'}->value / 100;
+            }
+
             $ticketsInfo[$key][] = $ticketInfo;
         }
 
