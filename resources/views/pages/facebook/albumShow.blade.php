2885d620a0e2d069868bff1a77a8019232195576
Jonathan Robinson <jayrowe86@gmail.com>
2016-08-15 21:43:07 -0500

    add facebook share button and attributes loggin

--- resources/views/pages/facebook/albumShow.blade.php
+++ resources/views/pages/facebook/albumShow.blade.php
@@ -1,8 +1,21 @@
 @extends('layouts.master')
 @section('top_javascript')
     <link href="{{ asset('assets/css/lightbox.css') }}" rel="stylesheet" type="text/css">
+    <meta property="og:url"           content="{{ request()->fullUrl()}}" />
+    <meta property="og:type"          content="website" />
+    <meta property="og:title"         content="{{ "AAULYP | ".$album->name }}" />
+    <meta property="og:description"   content="{{ $album->description }}" />
+    <meta property="og:image"         content="{{ $album->photos[0]->image[1]->source or asset("assets/img/800x550/800x550.jpg") }}" />
 @stop
 @section('content')
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=1604453216535744";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
     <div class="page-content">
         <div class="container">
             <div class="row">
@@ -15,6 +28,8 @@
                         <h4>Where</h4>
                         <p> {{ $album->location }}</p>
                     @endif
+
+                    <div class="fb-share-button" data-href="{{ request()->fullUrl() }}" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Faaulyp.org%2F&amp;src=sdkpreparse">Share</a></div>
                 </div>
                 <div class="col-sm-6 col-md-8 gallery">
                     <div class="row">
