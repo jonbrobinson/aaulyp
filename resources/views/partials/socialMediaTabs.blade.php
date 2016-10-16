f5d7d0949f15ba71e5c8a613b9590e2c6153c230
Jonathan Robinson <jayrowe86@gmail.com>
2016-10-16 01:19:31 -0500

    add social media tabs partial

---
+++ resources/views/partials/socialMediaTabs.blade.php
@@ -0,0 +1,37 @@
+<!-- tabbed content -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1604453216535744";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+<div class="widget">
+    <div class="pull-right">
+    </div>
+    <div class="clearfix"></div>
+    <div class="tabpanel">
+        <!-- nav tabs -->
+        <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation" class="active"><a href="#content-popular" aria-controls="content-popular" role="tab" data-toggle="tab"><i class="fa fa-twitter"></i> Twitter</a></li>
+            <li role="presentation"><a href="#content-recents" aria-controls="content-recents" role="tab" data-toggle="tab"><i class="fa fa-facebook"></i> Facebook</a></li>
+            {{--<li role="presentation"><a href="#content-recent-comments" aria-controls="content-recent-comments" role="tab" data-toggle="tab"><i class="fa fa-twitter"></i></a></li>--}}
+        </ul>
+        <!-- end nav tabs -->
+        <!-- tab panes -->
+        <div class="tab-content">
+            <div id="content-popular" class="tab-pane fade in active" role="tabpanel">
+                <a class="twitter-timeline" href="https://twitter.com/AAULYP" data-widget-id="731769931243495424">Tweets by @AAULYP</a>
+            </div>
+            <div id="content-recents" class="tab-pane fade" role="tabpanel">
+                <div class="fb-page" data-href="https://www.facebook.com/AAULYP/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-height="300"><blockquote cite="https://www.facebook.com/AAULYP/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AAULYP/">Austin Area Urban League Young Professionals</a></blockquote></div>
+            </div>
+            {{--<div id="content-recent-comments" class="tab-pane fade" role="tabpanel">--}}
+                {{--<a class="twitter-timeline" href="//twitter.com/AAULYP" data-widget-id="730989146307600386">Tweets by @AAULYP</a>--}}
+            {{--</div>--}}
+        </div>
+        <!-- end tab panes -->
+    </div>
+</div>
+<!-- end tabbed content -->
