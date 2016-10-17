d3558696f04c852b5d39f94e5d775963678a01bf
Jonathan Robinson <jayrowe86@gmail.com>
2016-10-17 01:04:37 -0500

    quick changes to test

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -34,7 +34,7 @@
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">ABOUT <i class="fa fa-angle-down"></i></a>
                     <ul class="dropdown-menu" role="menu">
                         <li><a href="{{ url('/aaulyp') }}">AAULYP History</a></li>
-                        <li><a href="{{ url('/board') }}">Meet the YP Officers</a></li>
+                        {{--<li><a href="{{ url('/board') }}">Meet the YP Officers</a></li>--}}
                         <li><a href="{{ url('/aaul') }}">Austin Area Urban League</a></li>
                     </ul>
                 </li>
