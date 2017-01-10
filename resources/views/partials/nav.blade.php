d98d5f85df10ad159ad6f806fe7944955b68d93c
Jonathan Robinson <jayrowe86@gmail.com>
2017-01-09 22:35:22 -0600

    update our history

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -36,7 +36,7 @@
                 <li class="dropdown ">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">ABOUT US<i class="fa fa-angle-down"></i></a>
                     <ul class="dropdown-menu" role="menu">
-                        <li><a href="{{ url('/aaulyp') }}">History</a></li>
+                        <li><a href="{{ url('/aaulyp') }}">Our History</a></li>
                         {{--<li><a href="{{ url('/board') }}">Meet the YP Officers</a></li>--}}
                         <li><a href="{{ url('http://www.aaul.org') }}">Austin Area Urban League</a></li>
                         <li><a href="{{ url('http://www.nulyp.org') }}">National Urban League Young Professionals</a></li>
