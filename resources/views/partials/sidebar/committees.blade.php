f3460df987f558cc8057dc9857cf8aa6b6423695
Jonathan Robinson <jonrobinson.codes@gmail.com>
2017-02-26 11:10:25 -0600

    remove fundrasing committeee links

--- resources/views/partials/sidebar/committees.blade.php
+++ resources/views/partials/sidebar/committees.blade.php
@@ -3,7 +3,7 @@
     <li class="{{ $communication  or "list-group-item"}}"><a href="/committee/communication">Communication</a></li>
     <li class="{{ $community  or "list-group-item"}}"><a href="/committee/community">Community Outreach</a></li>
     <li class="{{ $development  or "list-group-item"}}"><a href="/committee/development">Professional Development</a></li>
-    <li class="{{ $fundraising  or "list-group-item"}}"><a href="/committee/fundraising">Fundraising</a></li>
+{{--    <li class="{{ $fundraising  or "list-group-item"}}"><a href="/committee/fundraising">Fundraising</a></li>--}}
     <li class="{{ $membership  or "list-group-item"}}"><a href="/committee/membership">Membership | Social</a></li>
 
 </ul>
