47e89be5f72c09ce8c809c9bd8b7093815f9282c
Jonathan Robinson <jonrobinson.codes@gmail.com>
2017-12-16 17:20:27 -0500

    halt email  subscriptions

--- public/assets/js/mailchimpyp.js
+++ public/assets/js/mailchimpyp.js
@@ -2,16 +2,24 @@ $(document).ready( function() {
     var form = $('#mc-embedded-subscribe-form');
     if ( form.length > 0) {
         form.submit(function(e) {
-            var inputValue = form.find(':input').val();
-
-            var valid = true;
-
-            if(!/(.com)|(.net)|(.org)|(.edu)$/.test(inputValue)) {
-                valid = false;
-            }
-            if(!valid) {
-                e.preventDefault(); // avoid to execute the actual submit of the form.
-            }
+            e.preventDefault(); // avoid to execute the actual submit of the form.
+            //
+            //var inputValue = form.find(':input').val();
+            //
+            //var valid = true;
+            //
+            //if(!/(.com)|(.net)|(.org)|(.edu)$/.test(inputValue)) {
+            //    valid = false;
+            //}
+            //
+            //if(/(mail.ru)|(bk.ru)|(.org)|(.edu)$/.test(inputValue)) {
+            //    valid = false;
+            //}
+            //
+            //
+            //if(!valid) {
+            //    e.preventDefault(); // avoid to execute the actual submit of the form.
+            //}
         });
     }
 });
