8020a0e015f3915d86a3e0c7de94ac05bafe14a4
Jonathan Robinson <jonrobinson.codes@gmail.com>
2017-12-17 12:40:59 -0500

    update mc group

--- public/assets/js/mailchimpyp.js
+++ public/assets/js/mailchimpyp.js
@@ -2,24 +2,27 @@ $(document).ready( function() {
     var form = $('#mc-embedded-subscribe-form');
     if ( form.length > 0) {
         form.submit(function(e) {
-            e.preventDefault(); // avoid to execute the actual submit of the form.
-            //
-            //var inputValue = form.find(':input').val();
-            //
-            //var valid = true;
-            //
-            //if(!/(.com)|(.net)|(.org)|(.edu)$/.test(inputValue)) {
-            //    valid = false;
-            //}
-            //
-            //if(/(mail.ru)|(bk.ru)|(.org)|(.edu)$/.test(inputValue)) {
-            //    valid = false;
-            //}
-            //
-            //
-            //if(!valid) {
-            //    e.preventDefault(); // avoid to execute the actual submit of the form.
-            //}
+
+            var inputValue = form.find(':input').val();
+            var hiddenValue = $('#mch').val();
+            console.log(hiddenValue);
+            var valid = true;
+
+            if(!/(.com)|(.net)|(.org)|(.edu)$/.test(inputValue)) {
+                valid = false;
+            }
+
+            if(/(mail.ru)|(bk.ru)|(inbox.ru)$/.test(inputValue)) {
+                valid = false;
+            }
+
+            if(hiddenValue){
+                valid = false;
+            }
+
+            if(!valid) {
+                e.preventDefault(); // avoid to execute the actual submit of the form.
+            }
         });
     }
 });
